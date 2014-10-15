fb8817d659abfb5dce8334a9354011de7751600d
Brendan Whitfield <brendanw@windworksdesign.com>
2014-10-14 22:51:31 -0400

    Catch OS no file error (for running on desktops)

--- obd_utils.py
+++ obd_utils.py
@@ -1,32 +1,44 @@
 import serial
 import platform
+import errno
+
+# returns boolean for port availability
+def tryPort(portStr):
+	try:
+		s = serial.Serial(portStr)
+		s.close()   # explicit close 'cause of delayed GC in java
+		return True
+
+	except serial.SerialException:
+		pass
+	except OSError, e:
+		if e.errno != errno.ENOENT: # permit "no such file or directory" errors
+			raise e
+
+	return False
+
 
 def scanSerial():
-    """scan for available ports. return a list of serial names"""
-    available = []
- # Enable Bluetooh connection
-    for i in range(10):
-      try:
-		s = serial.Serial("/dev/rfcomm"+str(i))
-		available.append( (str(s.port)))
-		s.close()   # explicit close 'cause of delayed GC in java
-      except serial.SerialException:
-		pass
- # Enable USB connection
-    for i in range(256):
-      try:
-        s = serial.Serial("/dev/ttyUSB"+str(i))
-        available.append(s.portstr)
-        s.close()   # explicit close 'cause of delayed GC in java
-      except serial.SerialException:
-        pass
- # Enable obdsim 
-    #for i in range(256):
-      #try: #scan Simulator
-        #s = serial.Serial("/dev/pts/"+str(i))
-        #available.append(s.portstr)
-        #s.close()   # explicit close 'cause of delayed GC in java
-      #except serial.SerialException:
-        #pass
-    
-    return available
+	"""scan for available ports. return a list of serial names"""
+	available = []
+	# Enable Bluetooh connection
+	for i in range(10):
+		portStr = "/dev/rfcomm%d" % i
+		if tryPort(portStr):
+			available.append(portStr)
+
+	# Enable USB connection
+	for i in range(256):
+		portStr = "/dev/ttyUSB%d" % i
+		if tryPort(portStr):
+			available.append(portStr)
+
+	# Enable obdsim
+	'''
+	for i in range(256):
+		portStr = "/dev/pts/%d" % i
+		if tryPort(portStr):
+			available.append(portStr)
+	'''
+	
+	return available
