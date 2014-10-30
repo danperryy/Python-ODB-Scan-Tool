5ab751075667c8cd1ac27101280773333425d43d
Brendan Whitfield <brendanw@windworksdesign.com>
2014-10-30 13:10:36 -0400

    added GPL header to __init__

--- obd/__init__.py
+++ obd/__init__.py
@@ -1,4 +1,26 @@
 
+########################################################################
+#                                                                      #
+# python-OBD: A python OBD-II serial module derived from pyobd         #
+# Copyright 2014 Brendan Whitfield                                     #
+#                                                                      #
+########################################################################
+#                                                                      #
+# python-OBD is free software: you can redistribute it and/or modify   #
+# it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation, either version 2 of the License, or    #
+# (at your option) any later version.                                  #
+#                                                                      #
+# python-OBD is distributed in the hope that it will be useful,        #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
+# GNU General Public License for more details.                         #
+#                                                                      #
+# You should have received a copy of the GNU General Public License    #
+# along with python-OBD.  If not, see <http://www.gnu.org/licenses/>.  #
+#                                                                      #
+########################################################################
+
 from obd import OBD
 from commands import commands
 from utils import scanSerial
