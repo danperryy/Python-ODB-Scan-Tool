74208a0d6d4f5e10e0823647c7db56c0a2250ee9
Brendan Whitfield <brendanw@windworksdesign.com>
2014-12-16 00:10:50 -0500

    bump to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="obd",
-    version="0.1.0",
+    version="0.2.0",
     description=("Serial module for handling live sensor data from a vehicle's OBD-II port"),
     classifiers=[
         "Operating System :: POSIX :: Linux",
