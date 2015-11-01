2c7828ebd103da4fe20b3292b5595a07b214b061
Brendan Whitfield <brendanw@windworksdesign.com>
2015-11-01 00:11:29 -0400

    fixed tests directly accessing the old ECU_Map

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -1,7 +1,7 @@
 
 import random
 from obd.protocols import *
-from obd.protocols.protocol import Frame, Message, ECU_Map
+from obd.protocols.protocol import Frame, Message
 
 
 def test_ECU():
@@ -16,41 +16,6 @@ def test_ECU():
             assert (ecu & other_ecu) == 0, "ECU: %d has a conflicting bit with another ECU constant" %ecu
 
         tested.append(ecu)
-
-
-def test_ECU_Map():
-
-    # test simple default map
-    e = ECU_Map({
-        0 : 0,
-        1 : 10,
-        2 : 20
-    })
-
-    for tx_id in range(3):
-        ecu = (tx_id * 10)
-        assert e.resolve(tx_id) == ecu, "ECU_Map.resolve() failed"
-        assert e.lookup(ecu) == tx_id,  "ECU_Map.lookup() failed"
-
-    # test undefined tx_ids
-    assert e.resolve(3) == ECU.UNKNOWN, "ECU_Map.resolve() did not return ECU.UNKNOWN for undefined tx_id"
-
-    # test tx_id writting
-    e.set(3, 30)
-    assert e.resolve(3) == 30, "ECU_Map.set() failed after resolve()"
-    assert e.lookup(30) == 3,  "ECU_Map.set() failed after lookup()"
-
-    # test tx_id overwritting
-    e.set(3, 300)
-    assert e.resolve(3) == 300, "ECU_Map.set() failed after overwrite"
-    assert e.lookup(300) == 3,  "ECU_Map.set() failed after overwrite"
-
-    # test conflicting ECU ID values
-    # no two tx_ids should resolve to the same ECU ID
-    e.set(3, 0) # should cause tx_id 0 become ECU.UNKNOWN
-    assert e.resolve(3) == 0, "ECU_Map.set() after conflicting overwrite"
-    assert e.lookup(0) == 3,  "ECU_Map.set() after conflicting overwrite"
-    assert e.resolve(0) == ECU.UNKNOWN
 
 
 def test_frame():
@@ -86,19 +51,19 @@ def test_populate_ecu_map():
 
     # use primary ECU when multiple are present
     p = SAE_J1850_PWM(["48 6B 10 41 00 BE 1F B8 11 AA", "48 6B 12 41 00 BE 1F B8 11 AA"])
-    assert p.ecu_map.lookup(ECU.ENGINE) == 0x10
+    assert p.ecu_map[0x10] == ECU.ENGINE
 
     # use lone responses regardless
     p = SAE_J1850_PWM(["48 6B 12 41 00 BE 1F B8 11 AA"])
-    assert p.ecu_map.lookup(ECU.ENGINE) == 0x12
+    assert p.ecu_map[0x12] == ECU.ENGINE
 
     # if primary ECU is not listed, use response with most PIDs supported
     p = SAE_J1850_PWM(["48 6B 12 41 00 BE 1F B8 11 AA", "48 6B 14 41 00 00 00 B8 11 AA"])
-    assert p.ecu_map.lookup(ECU.ENGINE) == 0x12
+    assert p.ecu_map[0x12] == ECU.ENGINE
 
-    # if no messages were received, the defaults stay in place
+    # if no messages were received, then the map is empty
     p = SAE_J1850_PWM([])
-    assert p.ecu_map.lookup(ECU.ENGINE) == p.TX_ID_ENGINE
+    assert len(p.ecu_map) == 0
 
 
 def test_call_filtering():
